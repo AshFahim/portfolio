aaa4aac17d6e2d66c82739ff24b0926633c668a5
AshFahim <fahimashiqurrahman@gmail.com>
2024-05-27 22:22:07 +0600

    hero content bug fix

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -36,7 +36,7 @@ const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[20px] font-semibold">
-            Ashiqur Rhaman Fahim&apos;s Portfolio
+            Ashiqur Rhaman Fahim
           </h1>
         </motion.div>
 
