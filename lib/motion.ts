a1beebe6bac4e6ba0d7a102dc89ee1cf55e3c5c8
AshFahim <fahimashiqurrahman@gmail.com>
2025-08-13 03:44:16 +0600

    lib files

---
+++ lib/motion.ts
@@ -0,0 +1,99 @@
+export const fadeIn = (
+  direction: "left" | "right" | "up" | "down" | string,
+  type: string,
+  delay: number,
+  duration: number
+) => {
+  return {
+    hidden: {
+      x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+      y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const slideIn = (
+  direction: "left" | "right" | "up" | "down" | string,
+  type: string,
+  delay: number,
+  duration: number
+) => {
+  return {
+    hidden: {
+      x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+      y: direction === "up" ? "100%" : direction === "down" ? "-100%" : 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const staggerContainer = (
+  staggerChildren?: number,
+  delayChildren?: number
+) => {
+  return {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: staggerChildren,
+        delayChildren: delayChildren || 0,
+      },
+    },
+  };
+};
+
+export const skillContainerVariant = () => {
+  return {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+      },
+    },
+  };
+};
+
+export const skillIconVariant = () => {
+  return {
+    hover: { y: -5, transition: { yoyo: Infinity, duration: 0.5 } },
+  };
+};
+
+export const lockVariants = () => {
+  return {
+    hidden: { y: 0 },
+    visible: {
+      y: [0, -10, 0],
+      transition: {
+        repeat: Infinity,
+        duration: 2,
+        ease: "easeInOut",
+      },
+    },
+  };
+};
