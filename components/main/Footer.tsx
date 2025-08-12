10b5492f266df286b7d4b1b1b83f60091448967c
AshFahim <fahimashiqurrahman@gmail.com>
2025-08-13 03:44:56 +0600

    updated socials

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -1,83 +1,35 @@
-import React from "react";
-import {
-  RxDiscordLogo,
-  RxGithubLogo,
-  RxInstagramLogo,
-  RxTwitterLogo,
-} from "react-icons/rx";
-import { FaDiscord, FaLinkedin, FaMailBulk, FaYoutube } from "react-icons/fa";
+import Image from "next/image";
+import { USED_TECH } from "@/constants";
 
-const Footer = () => {
+export const Footer = () => {
   return (
-    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px]">
+    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg md:p-[15px]">
       <div className="w-full flex flex-col items-center justify-center m-auto">
-        <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
-          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-            <div className="font-bold text-[16px]">Contact me</div>
-            <a
-              href="mailto:fahimashiqurrahman@gmail.com"
-              className="flex flex-row items-center my-[15px] cursor-pointer"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaMailBulk />
-              <span className="text-[15px] ml-[6px]">
-                Email: fahimashiqurrahman@gmail.com
-              </span>
-            </a>
-            <a
-              href="https://github.com/AshFahim"
-              className="flex flex-row items-center my-[15px] cursor-pointer"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <RxGithubLogo />
-              <span className="text-[15px] ml-[6px]">Github</span>
-            </a>
-            <a
-              href="https://discord.com/invite/yourinvitecode"
-              className="flex flex-row items-center my-[15px] cursor-pointer"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <RxDiscordLogo />
-              <span className="text-[15px] ml-[6px]">Discord</span>
-            </a>
-          </div>
-          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-            <div className="font-bold text-[16px]">Social Media</div>
-            <a
-              href="https://www.instagram.com/yourusername"
-              className="flex flex-row items-center my-[15px] cursor-pointer"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <RxInstagramLogo />
-              <span className="text-[15px] ml-[6px]">Instagram</span>
-            </a>
-            <a
-              href="https://twitter.com/yourusername"
-              className="flex flex-row items-center my-[15px] cursor-pointer"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <RxTwitterLogo />
-              <span className="text-[15px] ml-[6px]">Twitter</span>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/yourusername"
-              className="flex flex-row items-center my-[15px] cursor-pointer"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLinkedin />
-              <span className="text-[15px] ml-[6px]">LinkedIn</span>
-            </a>
+        <div className="flex md:flex-row flex-col justify-between items-center space-y-4 md:space-y-0 my-auto w-full md:w-3/4 ">
+          <p className="mb-[20px] text-[20px]">
+            &copy; Ashiqur Rahman Fahim {new Date().getFullYear()} Inc. All rights
+            reserved.
+          </p>
+          <div className="flex flex-row space-x-2">
+            <p className="mb-[20px] text-[20px] text-white">Made with </p>
+            {USED_TECH.map((skill) => (
+              <div key={skill.name} className="w-6 relative group">
+                <Image
+                  src={`/skills/${skill.image}`}
+                  width={skill.size}
+                  height={skill.size}
+                  alt={skill.name}
+                  draggable={false}
+                  className="cursor-pointer select-none"
+                />
+                <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-xs rounded-sm py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
+                  {skill.name}
+                </span>
+              </div>
+            ))}
           </div>
         </div>
       </div>
     </div>
   );
 };
-
-export default Footer;
